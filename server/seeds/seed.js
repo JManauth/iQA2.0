8ba0006c6452b81c06d84b30967456822027b673
jafet <jafet.manauth@hotmail.com>
2021-10-08 02:04:54 -0500

    created users seed

---
+++ server/seeds/seed.js
@@ -0,0 +1,15 @@
+const db = require('../config/connection');
+const { User, Evaluation } = require('../models');
+
+const userData = require('./userData.json');
+// const evaluationData = require('./evaluationData.json');
+
+db.once('open', async () => {
+    await User.deleteMany({});
+    // await Evaluation.deleteMany({});
+
+    const users = await User.insertMany(userData);
+    // const evals = await Evaluation.insertMany(evaluationData);
+    console.log('all done!');
+    process.exit(0);
+})
